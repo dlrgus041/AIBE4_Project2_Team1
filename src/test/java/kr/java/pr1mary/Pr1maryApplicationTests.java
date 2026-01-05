8dec1f57988b2793d93951bca345d87279288852
tmddn7475 <junseungu74@naver.com>
2026-01-05 09:33:13 +0900

    first commit

---
+++ src/test/java/kr/java/pr1mary/Pr1maryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.java.pr1mary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr1maryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
